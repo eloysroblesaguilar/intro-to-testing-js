04bbc0b73a63b0f85f103285aae6aeda3c166282
Eloys Robles Aguilar <roblesaguilar.eloys@gmail.com>
2021-10-15 12:10:53 -0500

    completed exercise 10

--- code.js
+++ code.js
@@ -1,4 +1,13 @@
-// helloWorld function 
+// helloWorld function
 function helloWorld() {
     return "Hello, World!";
+}
+/** S Y N T A X **/
+
+function sayHello(input) {
+    return 'Hello ' + input;
+}
+
+function isFive(x) {
+    return x === 5;
 }
